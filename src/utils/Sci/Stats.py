de67faa61f9cedd29b576d06b2e72f5c1b70e244
yiakwy <yiak.wy@gmail.com>
2014-11-06 10:46:39 +0800

    Modify Neuro Module
    :x
    
    :x
    :q

--- src/utils/Sci/Stats.py
+++ src/utils/Sci/Stats.py
@@ -4,7 +4,7 @@ Created on 30 Oct, 2014
 
 @author: wangyi
 '''
-def neuro_train(x, y, timeseries):
+def neuro_train(x, y, timeseries, name):
     import neurolab as nl
     import numpy as np
     
@@ -19,20 +19,20 @@ def neuro_train(x, y, timeseries):
 
     tar = (tar - mean_tar) / (1.5 * std_tar)
     
-    y2 = np.zeros(size)
+    inp2 = np.zeros(size)
     # add another feature
     for i, k in enumerate(x):
-        y2[i] = tar[i - 1][0] 
+        inp2[i] = tar[i - 1][0] 
         
-    y2 = y2.reshape(size,1)
+    inp2 = inp2.reshape(size,1)
     
-    inp = np.concatenate((inp,y2), axis=1)           
+    inp = np.concatenate((inp,inp2), axis=1)           
     
     # Create network with 3 layers
     net = nl.net.newff([[0, 24], [0, 31], [0, 6], [0, 1]],[10, 10, 1])
     # Train network
     error = net.train(inp, tar, epochs=500, show=100, goal=0.02)
-    net.save('model2')
+    net.save('model_' + name)
 
     # Plot result
     import pylab as pl
@@ -42,7 +42,7 @@ def neuro_train(x, y, timeseries):
     pl.ylabel('error (default SSE)')
     pl.show()
     
-def neuro_train_validation(x, y, timeseries):
+def neuro_validate(x, y, timeseries, name):
     import neurolab as nl
     import numpy as np
     
@@ -58,16 +58,16 @@ def neuro_train_validation(x, y, timeseries):
     tar = (tar - mean_tar) / (1.5 * std_tar)
     
     # add another feature
-    y2 = np.zeros(size)
+    inp2 = np.zeros(size)
     # add another feature
     for i, k in enumerate(y):
-        y2[i] = tar[i - 1][0] 
+        inp2[i] = tar[i - 1][0] 
         
-    y2 = y2.reshape(size,1)
+    inp2 = inp2.reshape(size,1)
     
-    inp = np.concatenate((inp,y2), axis=1)  
+    inp = np.concatenate((inp,inp2), axis=1)  
     
-    net = nl.load('model2')
+    net = nl.load('model' + name)
     # Simulate network
     out = net.sim(inp).reshape(size)
     
@@ -81,7 +81,7 @@ def neuro_train_validation(x, y, timeseries):
     pl.legend(['train target', 'net output'])
     pl.show()
 
-def neuro_predict(x0, y0, y):
+def neuro_predict(x0, y0, y, name):
     import neurolab as nl 
     import numpy as np 
     
@@ -94,7 +94,7 @@ def neuro_predict(x0, y0, y):
     mean_tar = tar.mean(axis=0)[0]
     std_tar = tar.std(axis=0)[0] 
     
-    net = nl.load('model2')
+    net = nl.load('model' + name)
     
     out = net.sim(inp).reshape(size)
     
